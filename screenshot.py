ba256adb06d3de02ec920bf8b4f663462488b3ff
jericjan <cantilfrederick@gmail.com>
2024-01-11 19:10:44 +0800

    screenshot.py that i never committed apparently

--- screenshot.py
+++ screenshot.py
@@ -10,6 +10,50 @@ from PIL import Image
 from feature_match import find_match
 
 
+def screenshot(window_name):
+    hwnd = win32gui.FindWindowEx(None, None, "UnityWndClass", window_name)
+    print(f"Window ID: {hwnd}", end=", ")
+    class_name = win32gui.GetClassName(hwnd)
+    print(f"Class name: {class_name}", end=", ")
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    # left, top, right, bot = win32gui.GetClientRect(hwnd)
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bot - top
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    # result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
+    print(f"PrintWindow result: {result}")
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        "RGB", (bmpinfo["bmWidth"], bmpinfo["bmHeight"]), bmpstr, "raw", "BGRX", 0, 1
+    )
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        return im
+
+
 def screenshot_window_check(window_name):
     hwnd = win32gui.FindWindowEx(None, None, "UnityWndClass", window_name)
     print(f"Window ID: {hwnd}", end=", ")
